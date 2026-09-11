dab1cb6b3ed82b89d5ec90c4ec3e371305f8665c
Manya Singh <manyasingh264@gmail.com>
2026-09-11 20:06:03 +0530

    Register and Login API Implementation

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
 const express=require("express");
 const app = express();
+const cookieParser = require("cookie-parser")
+
+
+app.use(express.json())
+app.use(cookieParser())
+
+/**
+ * - Routes required
+ */
+const authRouter = require("./routes/auth.routes")
+app.use("/api/auth", authRouter)
+
 module.exports=app;
