394caa8610b7ee7f7e1d1211dc926a28c630a24e
Manya Singh <manyasingh264@gmail.com>
2026-09-12 23:01:59 +0530

    Created Logout API

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -39,13 +39,13 @@ async function authSystemUserMiddleware(req, res, next) {
         })
     }
 
-    // const isBlacklisted = await tokenBlackListModel.findOne({ token })
+    const isBlacklisted = await tokenBlackListModel.findOne({ token })
 
-    // if (isBlacklisted) {
-    //     return res.status(401).json({
-    //         message: "Unauthorized access, token is invalid"
-    //     })
-    // }
+    if (isBlacklisted) {
+        return res.status(401).json({
+            message: "Unauthorized access, token is invalid"
+        })
+    }
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
