7e1f7ae0a9042c3132876b7868f6699cb96f0fee
Manya Singh <manyasingh264@gmail.com>
2026-09-12 11:27:11 +0530

    Created function to send emails using NodeMailer and created Account models,routes and controllers architecture

--- src/app.js
+++ src/app.js
@@ -10,6 +10,11 @@ app.use(cookieParser())
  * - Routes required
  */
 const authRouter = require("./routes/auth.routes")
+const accountRouter = require("./routes/account.routes")
+
+
+//use routes
 app.use("/api/auth", authRouter)
+app.use("/api/accounts", accountRouter)
 
 module.exports=app;
